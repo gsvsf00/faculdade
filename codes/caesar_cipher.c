320b0d2d3d828365941595727fa9e3cabff9635a
Gabriel Vasconcelos <gabriels.v9@gmail.com>
2020-03-06 16:16:50 -0300

    Update

--- codes/caesar_cipher.c
+++ codes/caesar_cipher.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
-int main()
-{
+void criptografar(){
 	char crip[100], aux;
 	int i;
-	
+
 	printf("Digite: ");
-	gets(crip);
+		gets(crip);
 	
-	for(i = 0; crip[i] != '\0'; ++i){
+		for(i = 0; crip[i] != '\0'; ++i){
 		aux = crip[i];
 		
 		if(aux >= 'a' && aux <= 'z'){
@@ -30,8 +29,60 @@ int main()
 			crip[i] = aux;
 		}
 	}
+		printf("Criptado: %s", crip);
+}
+void descriptografar(){
+	char crip[100], aux;
+	int i;
+
+	printf("Digite: ");
+		gets(crip);
 	
-	printf("Criptado: %s", crip);
-	
+		for(i = 0; crip[i] != '\0'; ++i){
+		aux = crip[i];
+		
+		if(aux >= 'a' && aux <= 'z'){
+			aux = aux - 3;
+			
+			if(aux > 'z'){
+				aux = aux - 'z' - 'a' + 1;
+			}
+			
+			crip[i] = aux;
+		}
+		else if(aux >= 'A' && aux <= 'Z'){
+			aux = aux - 3;
+			
+			if(aux > 'Z'){
+				aux = aux - 'Z' - 'A' + 1;
+			}
+			
+			crip[i] = aux;
+		}
+	}
+		printf("Criptado: %s", crip);
+}
+
+
+int main()
+{
+	int op;
+	printf("Digite a opcao:\n\n1- Criptografar\n2-Descriptografar\n\n");
+	scanf ("%d", &op);
+	switch (op)
+	{
+	case 1:
+		criptografar();
+	break;
+	case 2:
+		descriptografar();
+	break;
+
+	default:
+		printf ("Valor invalido!\n");
+	break;
+	}
+
+	getchar();
 	return 0;
 }

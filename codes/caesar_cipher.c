09a7dbe491121803abd5c2faaa08027a0ace77c5
Gabriel Vasconcelos <gabriels.v9@gmail.com>
2020-03-11 15:58:11 -0300

    Update

--- codes/caesar_cipher.c
+++ codes/caesar_cipher.c
@@ -32,34 +32,6 @@ void criptografar(){
 	}
 		printf("Criptado: %s", crip);
 }
-/*void descriptografar(char crip[]){
-	char aux;
-	int i;
-	
-		for(i = 0; crip[i] != '\0'; ++i){
-		aux = crip[i];
-		
-		if(aux >= 'a' && aux <= 'z'){
-			aux = aux - 3;
-			
-			if(aux > 'z'){
-				aux = aux - 'z' - 'a' + 1;
-			}
-			
-			crip[i] = aux;
-		}
-		else if(aux >= 'A' && aux <= 'Z'){
-			aux = aux - 3;
-			
-			if(aux > 'Z'){
-				aux = aux - 'Z' - 'A' + 1;
-			}
-			
-			crip[i] = aux;
-		}
-	}
-		printf("Descriptado: %s", &crip);
-}*/
 
 void descriptografar(){
 

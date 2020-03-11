90649fa057b88b39487d654507b6cbf92dd62ebb
gsv99 <40930513+gsv99@users.noreply.github.com>
2020-03-11 15:30:08 -0300

    Update

--- codes/caesar_cipher.c
+++ codes/caesar_cipher.c
@@ -1,41 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void criptografar();
-void descriptografar();
-void teste(){
-	printf("Teste");
-}
-int main()
-{
-	int op;
-	printf("Digite a opcao:\n\n1- Criptografar\n2-Descriptografar\n\n");
-	scanf ("%d", &op);
-	switch (op)
-	{
-	case 1:
-		criptografar();
-	break;
-	case 2:
-		descriptografar();
-	break;
-	case 3:
-		teste();
-	break;
+char crip[50],aux;
+int i;
 
-	default:
-		printf ("Valor invalido!\n");
-	break;
-	}
-
-	return 0;
+void frase() {
+	printf("Enter a String:\t");
+	scanf("%[^\n]s", crip);
 }
 
 void criptografar(){
-	char crip[100], aux;
-	int i;
-	
-	printf("Digite: ");
-		gets(crip);
 	
 		for(i = 0; crip[i] != '\0'; ++i){
 		aux = crip[i];
@@ -46,27 +20,21 @@ void criptografar(){
 			if(aux > 'z'){
 				aux = aux - 'z' + 'a' - 1;
 			}
-			
 			crip[i] = aux;
 		}
 		else if(aux >= 'A' && aux <= 'Z'){
-			aux = aux + 3;
-			
+			aux = aux + 3;			
 			if(aux > 'Z'){
 				aux = aux - 'Z' + 'A' - 1;
 			}
-			
 			crip[i] = aux;
 		}
 	}
 		printf("Criptado: %s", crip);
 }
-void descriptografar(){
-	char crip[100], aux;
+/*void descriptografar(char crip[]){
+	char aux;
 	int i;
-
-	printf("Digite: ");
-		gets(crip);
 	
 		for(i = 0; crip[i] != '\0'; ++i){
 		aux = crip[i];
@@ -90,5 +58,54 @@ void descriptografar(){
 			crip[i] = aux;
 		}
 	}
-		printf("Criptado: %s", crip);
+		printf("Descriptado: %s", &crip);
+}*/
+
+void descriptografar(){
+
+		for(i = 0; crip[i] != '\0'; ++i){
+		aux = crip[i];
+		
+		if(aux >= 'a' && aux <= 'z'){
+			aux = aux - 3;
+			
+			if(aux > 'z'){
+				aux = aux - 'z' + 'a' - 1;
+			}
+			crip[i] = aux;
+		}
+		else if(aux >= 'A' && aux <= 'Z'){
+			aux = aux - 3;			
+			if(aux > 'Z'){
+				aux = aux - 'Z' + 'A' - 1;
+			}
+			crip[i] = aux;
+		}
+	}
+		printf("Descriptado: %s", crip);
+}
+
+int main()
+{
+	int op;
+	frase();
+	
+	while (1){
+	printf("Digite a opcao:\n\n1- Criptografar\n2-Descriptografar\n3-Sair\n");
+	scanf ("%d", &op);
+	switch (op){
+	case 1:
+		criptografar();
+	break;
+	case 2:
+		descriptografar();
+	break;
+	case 3:
+			exit(0);
+	default:
+		printf ("Valor invalido!\n");
+	break;
+		}
+	}
+	return 0;
 }
